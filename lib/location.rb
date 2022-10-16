f19533c25f03f43d4b1c2f38fcdb3e166fb6f91c
peterchen999 <peter.chen999@gmail.com>
2022-10-16 22:24:43 +0800

    bugfix - fixed select to list

--- lib/location.rb
+++ lib/location.rb
@@ -7,16 +7,16 @@ module TravellingSuggestions
       @location_data = parse_location(data)
     end
     def parse_location(data)
-      data['records']['location'].select{|data| data['locationName'] == @name}
+      data['records']['location'].select{|data| data['locationName'] == @name}[0]
     end
     def prob_rain
-      @location_data['weatherElement'].select{|data| data['elementName'] == 'PoP'}
+      @location_data['weatherElement'].select{|data| data['elementName'] == 'PoP'}[0]
     end
     def min_temperature
-        @location_data['weatherElement'].select{|data| data['elementName'] == 'minT'}
+        @location_data['weatherElement'].select{|data| data['elementName'] == 'minT'}[0]
     end
     def max_temperature
-        @location_data['weatherElement'].select{|data| data['elementName'] == 'maxT'}
+        @location_data['weatherElement'].select{|data| data['elementName'] == 'maxT'}[0]
     end
   end
 end
