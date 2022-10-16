bdfa78a9d80f76da62314908435f61b940802e44
everace <daybreakli1226@gmail.com>
2022-10-16 23:19:30 +0800

    add more tests

--- spec/cwb_api_spec.rb
+++ spec/cwb_api_spec.rb
@@ -14,11 +14,14 @@ CORRECT = YML_FILE.select{|data| data['locationName'] == LOCATION}
 describe 'Tests CWB API library' do 
   describe 'Location information' do 
     it 'HAPPY: should provide correct location attributes' do 
-        location = TravellingSuggestions::CWBApi.new(CWB_TOKEN).location(LOCATION)
-        _(location.prob_rain).must_equal CORRECT[0]['weatherElement'].select{|data| data['elementName'] == 'PoP'}[0]
-        _(location.min_temperature).must_equal CORRECT[0]['weatherElement'].select{|data| data['elementName'] == 'MinT'}[0]
-        _(location.max_temperature).must_equal CORRECT[0]['weatherElement'].select{|data| data['elementName'] == 'MaxT'}[0]
+      location = TravellingSuggestions::CWBApi.new(CWB_TOKEN).location(LOCATION)
+      _(location.prob_rain).must_equal CORRECT[0]['weatherElement'].select{|data| data['elementName'] == 'PoP'}[0]
+      _(location.min_temperature).must_equal CORRECT[0]['weatherElement'].select{|data| data['elementName'] == 'MinT'}[0]
+      _(location.max_temperature).must_equal CORRECT[0]['weatherElement'].select{|data| data['elementName'] == 'MaxT'}[0]
     end
 
+    it 'SAD: should raise exception when unauthorized' do
+      _(proc do TravellingSuggestions::CWBApi.new('BAD_TOKEN').location(LOCATION) end).must_raise TravellingSuggestions::CWBApi::Errors::Unauthorized
+    end
   end
 end
