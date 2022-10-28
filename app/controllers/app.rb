0bca26f2ba681274fa36fef037fa5ed38b684bc6
everace <daybreakli1226@gmail.com>
2022-10-29 04:19:06 +0800

    modify - app.rb, require_app.rb, gateway_cwb_api_spec.rb
    -setup app.rb: plugins, routing home
    -modify change files' require relative because of moving lib folder

---
+++ app/controllers/app.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'roda'
+require 'slim'
+
+module TravellingSuggestions
+  class App < Roda
+    plugin :render, engine: 'slim', views: 'app/views'
+    plugin :assets, css: 'style.css', path: 'app/views/assets'
+    plugin :common_logger, $Stderr
+    plugin :halt
+
+    route do |routing|
+      routing.assets
+
+    roting.root do 
+      view 'home'
+    end
+
+  end
+end
+ 
