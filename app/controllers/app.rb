4e768042b173333f7e4188f153ec77c85650cd5f
peterchen999 <peter.chen999@gmail.com>
2022-10-30 22:43:32 +0800

    add - added /weather page and able to reply to user's location selection
    - modified app/controllers/app.rb, added routing.on 'weather'

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -3,6 +3,8 @@
 require 'roda'
 require 'slim'
 
+Slim::Engine.set_options encoding: "utf-8"
+
 module TravellingSuggestions
   class App < Roda
     plugin :render, engine: 'slim', views: 'app/views'
@@ -15,6 +17,29 @@ module TravellingSuggestions
 
       routing.root do 
         view 'home'
+      end
+
+      routing.on 'weather' do
+        routing.is do
+        # POST /weather/
+          routing.post do
+            location = routing.params['location']
+            routing.redirect "weather/#{location}"
+          end
+        end
+        routing.on String do |location|
+          routing.get do
+            if location == 'hsinchu'
+              location = '新竹縣'
+            elsif location == 'taipei'
+              location = '臺北市'
+            end
+            cwb_weather = TravellingSuggestions::CWB::LocationMapper
+              .new(CWB_TOKEN, TravellingSuggestions::CWB::CWBApi)
+              .find(location)
+            view 'weather', locals: { weather: cwb_weather }  
+          end
+        end
       end
     end
 
