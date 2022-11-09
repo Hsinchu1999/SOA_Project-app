41ef3654e9eefbf64c0873ceb093b4a25dc29839
peterchen999 <peter.chen999@gmail.com>
2022-11-09 22:29:09 +0800

    not finished

---
+++ app/models/repositories/locations.rb
@@ -0,0 +1,10 @@
+module TravellingSuggestions
+  module Repository
+    class Locations
+      def self.find_id(id)
+        rebuild_entity Database::LocationOrm.first(id: id)
+      end
+
+    end
+  end
+end
