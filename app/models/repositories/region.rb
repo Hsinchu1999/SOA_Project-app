4dddfa4ad2647575cf07340a1971f1c3e180f653
peterchen999 <peter.chen999@gmail.com>
2022-11-10 14:55:06 +0800

    new - new Repository object Region
    - new file region.rb for Repository::Region
    - generates the repository object of a particular region
    - still needs to decide whether contains a Entity::Weather object in Entity::Region

---
+++ app/models/repositories/region.rb
@@ -0,0 +1,31 @@
+module TravellingSuggestions
+    module Repository
+      class Region
+        def self.find_id(id)
+          rebuild_entity Database::RegionOrm.first(id: id)
+        end
+        
+        def self.rebuild_entity(db_record)
+          return nil unless db_record
+          
+          Entity::Region.new(
+            country:       db_record.country,
+            city:   db_record.city,
+            weather:    nil,        # still needs to decide
+          )
+        end
+
+        def self.rebuild_many_entities(db_records)
+          db_records.map do | db_member |
+            Repository::Region.rebuild_entity(db_member)
+          end
+        end
+
+        def self.db_find_or_create(entity)
+          nil
+        end
+        # to be completed
+
+      end
+    end
+  end
