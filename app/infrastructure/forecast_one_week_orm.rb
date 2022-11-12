80e8ddd4618a86c575aea59646c8490a1caa103c
peterchen999 <peter.chen999@gmail.com>
2022-11-12 19:47:27 +0800

    bugfix - fixed ForecastOneWeekOrm many_to_one key name

--- app/infrastructure/forecast_one_week_orm.rb
+++ app/infrastructure/forecast_one_week_orm.rb
@@ -8,25 +8,25 @@ module TravellingSuggestions
       many_to_one :region,
                   class: :'TravellingSuggestions::Database::RegionOrm'
       
-      many_to_one :first_day_id,
+      many_to_one :first_day,
                   class: :'TravellingSuggestions::Database::ForecastperDayOrm'
 
-      many_to_one :second_day_id,
+      many_to_one :second_day,
                   class: :'TravellingSuggestions::Database::ForecastperDayOrm'
 
-      many_to_one :third_day_id,
+      many_to_one :third_day,
                   class: :'TravellingSuggestions::Database::ForecastperDayOrm'
 
-      many_to_one :fourth_day_id,
+      many_to_one :fourth_day,
                   class: :'TravellingSuggestions::Database::ForecastperDayOrm'
 
-      many_to_one :fifth_day_id,
+      many_to_one :fifth_day,
                   class: :'TravellingSuggestions::Database::ForecastperDayOrm'
 
-      many_to_one :sixth_day_id,
+      many_to_one :sixth_day,
                   class: :'TravellingSuggestions::Database::ForecastperDayOrm'
 
-      many_to_one :seventh_day_id,
+      many_to_one :seventh_day,
                   class: :'TravellingSuggestions::Database::ForecastperDayOrm'
 
       plugin :timestamps, update_on_create: true
