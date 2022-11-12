11cd6ea7566ae6afc26419e719329aaca1fc66ff
peterchen999 <peter.chen999@gmail.com>
2022-11-12 16:45:47 +0800

    add - able to create table for forecast_per_day
    - added contents into 008_forecast_per_day_create.rb to create db
    - contains weather forecasts of a single day

---
+++ db/migrations/008_forecast_per_day_create.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:forecast_per_days) do
+      primary_key :id
+
+      Integer :pop
+      Integer :minT
+      Integer :maxT
+
+      DateTime :created_at
+      DateTime :updated_at
+    end
+  end
+end
