d6ddefc50c026890313293d3f208691dd1ff982a
peterchen999 <peter.chen999@gmail.com>
2022-11-12 17:46:21 +0800

    bugfix - fixed forecast_one_week table column name
    - modified 009_forecast_one_week_create.rb
    - added '_id' to name of foreign key to indicate using the id

--- db/migrations/009_forecast_one_week_create.rb
+++ db/migrations/009_forecast_one_week_create.rb
@@ -7,13 +7,13 @@ Sequel.migration do
     create_table(:forecast_one_weeks) do
       primary_key :id
       foreign_key :region_id, :regions, unique: true
-      foreign_key :first_day, :forecast_per_days, unique: true
-      foreign_key :second_day, :forecast_per_days, unique: true
-      foreign_key :third_day, :forecast_per_days, unique: true
-      foreign_key :fourth_day, :forecast_per_days, unique: true
-      foreign_key :fifth_day, :forecast_per_days, unique: true
-      foreign_key :sixth_day, :forecast_per_days, unique: true
-      foreign_key :seventh_day, :forecast_per_days, unique: true
+      foreign_key :first_day_id, :forecast_per_days, unique: true
+      foreign_key :second_day_id, :forecast_per_days, unique: true
+      foreign_key :third_day_id, :forecast_per_days, unique: true
+      foreign_key :fourth_day_id, :forecast_per_days, unique: true
+      foreign_key :fifth_day_id, :forecast_per_days, unique: true
+      foreign_key :sixth_day_id, :forecast_per_days, unique: true
+      foreign_key :seventh_day_id, :forecast_per_days, unique: true
       
       String :forecast_report_time
 
