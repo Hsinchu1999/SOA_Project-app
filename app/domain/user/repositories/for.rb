aff1ce54f4636a7573f133f6a932725ccc346ec1
everace2021 <daybreakli1226@gmail.com>
2022-11-24 01:08:31 +0800

    chores - setup domain folder

---
+++ app/domain/user/repositories/for.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative 'attraction'
+require_relative 'forecast_36hr'
+require_relative 'forecast_per_12hr'
+require_relative 'mbti_question'
+require_relative 'region'
+require_relative 'user'
+
+module TravellingSuggestions
+  module Repository
+    module For
+
+      ENTITY_REPOSITORY = {
+        Entity::Attraction => Attractions,
+        Entity::Forecast_36Hr => Forecasts_36Hr,
+        Entity::Forecast_Per_12Hr => Forecasts_Per_12Hr,
+        Entity::MBTI_Question => MBTI_Questions,
+        Entity::Region => Regions,
+        Entity::User => Users
+      }.freeze
+
+      def self.klass(entity_klass)
+        ENTITY_REPOSITORY[entity_klass]
+      end
+
+      def self.entity(entity_object)
+        ENTITY_REPOSITORY[entity_object.class]
+      end
+
+    end
+  end
+end
