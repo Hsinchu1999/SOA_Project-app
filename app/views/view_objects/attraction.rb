e0af117179e21adf98195c53e35d7dba31a5e97b
peterchen999 <peter.chen999@gmail.com>
2022-11-24 20:43:36 +0800

    new - added view object for Attraction
    - new file attraction.rb
    - contains functions to return each attr. of Attraction objects
    - returns a View object for Weather if needed

---
+++ app/views/view_objects/attraction.rb
@@ -0,0 +1,51 @@
+require_relative 'region'
+
+module Views
+  class Attraction
+    def initialize(attraction)
+      @attraction = attraction
+    end
+
+    def added_time
+      @attraction.added_time
+    end
+
+    def attraction_region
+      # returns a View object for Entity::Region object
+      Views::Region.new(@attraction.region)
+    end
+
+    def attraction_indoor_or_outdoor
+      @attraction.indoor_or_outdoor.to_s
+    end
+
+    def attraction_main_activity
+      @attraction.main_activity
+    end
+
+    def staying_time
+      @attraction.staying_time
+    end
+
+    def type
+      @attraction.type
+    end
+
+    def attendants
+      @attraction.attendants.to_s
+    end
+
+    def notes
+      @attraction.notes
+    end
+
+    def contact
+      @attraction.contact
+    end
+
+    def best_time_to_visit
+      @attraction.best_time_to_visit
+    end
+
+  end
+end
