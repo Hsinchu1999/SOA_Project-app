ea21180af980e7d69bc55d98dc0a973145708a81
peterchen999 <peter.chen999@gmail.com>
2022-12-14 17:55:29 +0800

    add - added api tests (mbti, adding user)
    - modified gateway_api_spec.rb
    - modified cassettes/ & coverage/
    - added HAPPY tests for mbti
    - added HAPPY tests (valid nicknames) and SAD tests (invalid nicknames)
      for adding user

--- spec/gateway_api_spec.rb
+++ spec/gateway_api_spec.rb
@@ -20,4 +20,34 @@ describe 'Tests CWB API library' do
       _(alive).must_equal true
     end
   end
+
+  # end
+  describe 'mbti tests' do
+    it 'should return mbti questions' do
+      MBTI_QUESTION_ID.each do |question_id|
+        question = TravellingSuggestions::Gateway::Api.new(TravellingSuggestions::App.config).list_mbti_question(question_id)
+
+        _(question.success?).must_equal true
+      end
+    end
+  end
+
+  describe 'Add user name' do
+    it 'should allow creating valid user nicknames' do
+      VALID_NICKNAMES.each do |nickname|
+        result = TravellingSuggestions::Gateway::Api.new(TravellingSuggestions::App.config).add_user(nickname, 'ENFJ')
+
+        unless result.success?
+          _(result.conflict?).must_equal true
+        end
+      end
+    end
+    it 'should deny creating invalid user nicknames' do
+      INVALID_NICKNAMES.each do |nickname|
+        result = TravellingSuggestions::Gateway::Api.new(TravellingSuggestions::App.config).add_user(nickname, 'ENFJ')
+
+        _(result.success?).must_equal false
+      end
+    end
+  end
 end
