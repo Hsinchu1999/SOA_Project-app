2e7b8ca5faaa937eeec012cdb45311cd891e627a
peterchen999 <peter.chen999@gmail.com>
2022-12-15 17:06:05 +0800

    modify - modified Service::ListUserFavorites
    - modified list_user_favorites.rb
    - call api and turns it into representer object

--- app/application/services/list_user_favorites.rb
+++ app/application/services/list_user_favorites.rb
@@ -6,7 +6,10 @@ module TravellingSuggestions
   module Service
     # A Service object for validating Entity::User object from db
     class ListUserFavorites
-      include Dry::Monads::Result::Mixin
+      include Dry::Transaction
+
+      step :call
+      step :reify_user_favorites
 
       def call(input)
         if (user = Repository::ForUser.klass(Entity::User).find_name(input[:nickname]))
@@ -25,6 +28,14 @@ module TravellingSuggestions
             )
           )
         end
+      end
+
+      def reify_user_favorites(input)
+        TravellingSuggestions::Representer::UserFavorite.new(OpenStruct.new)
+        .from_json(input)
+        .then{ |user_favorites| Success(user_favorites) }
+      rescue StandardError
+        Failure('Error in listing listing user\'s favorites, please try again')
       end
     end
   end
