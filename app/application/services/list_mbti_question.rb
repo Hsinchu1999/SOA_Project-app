93ba77f8749820a138dd8e2dee198da21405f72e
peterchen999 <peter.chen999@gmail.com>
2022-12-15 17:00:56 +0800

    modify - modified Service::ListMBTIQuestion service object
    - modified list_mbti_question.rb
    - calls api to get mbit question and turn it into Representer

--- app/application/services/list_mbti_question.rb
+++ app/application/services/list_mbti_question.rb
@@ -6,10 +6,13 @@ module TravellingSuggestions
   module Service
     # A Service object to validate Entity::Weather object from db
     class ListMBTIQuestion
-      include Dry::Monads::Result::Mixin
+      include Dry::Transaction
+
+      step :call
+      step :reify_mbti_question
 
       def call(question_id)
-        mbti_question = Repository::ForMBTI.klass(Entity::MBTIQuestion).find_id(question_id)
+        mbti_question = Gateway::Api.new(TravellingSuggestions::App.config).list_mbti_question(question_id)
         Success(
           Response::ApiResult.new(
             status: :ok,
@@ -23,6 +26,14 @@ module TravellingSuggestions
             message: 'Could not fetch mbti question from database'
           )
         )
+      end
+
+      def reify_mbti_question(input)
+        TravellingSuggestions::Representer::MBTIQuestion.new(OpenStruct.new)
+          .from_json(input)
+          .then{ |mbti_question| Success(mbti_question) }
+      rescue StandardError
+        Failure('Error in listing mbti questions, please try again')
       end
     end
   end
