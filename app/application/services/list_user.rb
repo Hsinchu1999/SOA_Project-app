ea77e7e8e20eb6013e3ba0eaed84f60b688b017e
peterchen999 <peter.chen999@gmail.com>
2022-12-15 17:21:13 +0800

    modify - modified Service::ListUser object
    - modified list_user.rb
    - calls api and check result

--- app/application/services/list_user.rb
+++ app/application/services/list_user.rb
@@ -6,10 +6,13 @@ module TravellingSuggestions
   module Service
     # A Service object for validating Entity::User object from db
     class ListUser
-      include Dry::Monads::Result::Mixin
+      include Dry::Transaction
+
+      step :call
+      step :reify_user
 
       def call(input)
-        if (user = Repository::ForUser.klass(Entity::User).find_name(input[:nickname]))
+        if (user = TravellingSuggestions::Gateway::Api.new(TravellingSuggestions::App.config).list_user(input[:nickname]))
           Success(
             Response::ApiResult.new(
               status: :ok,
@@ -24,6 +27,14 @@ module TravellingSuggestions
             )
           )
         end
+      end
+
+      def reify_user(input)
+        TravellingSuggestions::Representer::User.new(OpenStruct.new)
+        .from_json(input)
+        .then{ |user| Success(user) }
+      rescue StandardError
+        Failure('Error in listing user, please try again')
       end
     end
   end
