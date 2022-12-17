d75c3c027250c39034aa649a6e9b081580c98012
peterchen999 <peter.chen999@gmail.com>
2022-12-17 20:54:07 +0800

    add - added tests for gateway Gateway::Api.calculate_mbti_score
    - modified gateway_api_spec.rb
    - modified helpers/api_helper.rb
    - uses gateway to call api and check result
    - modified cassettes & coverage/

--- spec/gateway_api_spec.rb
+++ spec/gateway_api_spec.rb
@@ -3,6 +3,8 @@
 require_relative 'helpers/api_helper'
 require_relative 'helpers/vcr_helper'
 
+require 'json'
+
 describe 'Tests CWB API library' do
   VcrHelper.setup_vcr
 
@@ -39,6 +41,21 @@ describe 'Tests CWB API library' do
         ).list_mbti_question_set(set_size)
 
         _(question_set.success?).must_equal true
+      end
+    end
+  end
+
+  describe 'Get mbti test result' do
+    it 'should get correct test result' do
+      VALID_MBTI_QUESTION_PAIR.each_with_index do |question_pair, index|
+        result = TravellingSuggestions::Gateway::Api.new(
+          TravellingSuggestions::App.config
+        ).calculate_mbti_score(question_pair[0], question_pair[1])
+
+        _(result.success?).must_equal true
+
+        mbti_type = JSON.parse(result.payload)['personalities']
+        _(mbti_type).must_equal CORRECT_MBTI_QUESTION_RESULT[index]
       end
     end
   end
--- spec/helpers/api_helper.rb
+++ spec/helpers/api_helper.rb
@@ -17,6 +17,11 @@ require_app
 
 MBTI_QUESTION_ID = Array(1..10)
 MBTI_QUESTION_SET_SIZE = Array(1..11)
+VALID_MBTI_QUESTION_PAIR = [
+    [['1', '2', '3', '4'], ['A', 'B', 'A', 'B']],
+    [['9', '10', '11', '14'], ['B', 'A', 'A', 'A']]]
+CORRECT_MBTI_QUESTION_RESULT = %w[ENTJ ISFJ].freeze
+INVALID_MBTI_QUESTION_PAIR = ['0=A&1=A&2=A&3=A', '99=A&1=A&3=A&4=A'].freeze
 VALID_NICKNAMES = ['peter', 'Peter', 'Peter_', '_PETER', '_', 'PETERCHEN', 'peter_chen', 'peterchen999']
 INVALID_NICKNAMES = ['.', '', './']
 
