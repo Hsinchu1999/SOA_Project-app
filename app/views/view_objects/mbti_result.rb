1b361c772adbf2e81792a50d54c618929c1f0f49
peterchen999 <peter.chen999@gmail.com>
2022-12-17 21:46:05 +0800

    new - new Views object for mbti test result
    - new file view_objects/mbti_result.rb

---
+++ app/views/view_objects/mbti_result.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Views
+  # A Views object for a MBTI result open struct
+  class MBTIResult
+    def initialize(mbti_result)
+      @mbti_result = mbti_result
+    end
+
+    def personalities
+      @mbti_result.personalities
+    end
+  end
+end
+  
