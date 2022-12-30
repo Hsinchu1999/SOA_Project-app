dae1a9acb7b0128a05bc889a74df9b465c8e95db
peterchen999 <peter.chen999@gmail.com>
2022-12-30 21:57:59 +0800

    new - new Service object to handle attraction set
    - new file services/list_attraction_set.rb
    - calls api and use representer to reify result

---
+++ app/application/services/list_attraction_set.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'json'
+
+module TravellingSuggestions
+  module Service
+    # A Service object to get a set of attraction from api
+    class ListAttractionSet
+      include Dry::Transaction
+
+      step :call_api
+      step :reify_attraction_set
+
+      def call_api(set_size)
+        Gateway::Api.new(TravellingSuggestions::App.config)
+                    .list_attraction_set(set_size)
+                    .then do |result|
+          result.success? ? Success(result.payload) : Failure(result.message)
+        end
+      rescue StandardError
+        Failure('Could not access our API')
+      end
+
+      def reify_attraction_set(input)
+        TravellingSuggestions::Representer::AttractionSet.new(OpenStruct.new)
+                                                           .from_json(input)
+                                                           .then { |attraction_set| Success(attraction_set) }
+      rescue StandardError
+        Failure('Error in generating attraction set, please try again')
+      end
+    end
+  end
+end
